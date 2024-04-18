98cfe0a49412a6b7ac553a355107c1b0d868b84b
erudit aoue <philaoue@gmail.com>
2024-04-18 19:11:43 +0200

    le projet a commencé

--- weatherPhil/App.js
+++ weatherPhil/App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import about from "./composants/about.js"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>yo, érudit si tu vois ça, alors tu vois ma première application mobile </Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ weatherPhil/composants/about.js
@@ -0,0 +1,15 @@
+import {'react'} from "react";
+import {'view', 'text'} from "react-native";
+
+export default function About () {
+
+render(){
+return (
+
+<view>
+<text>A propos de moi</text>
+<text>lorem ipsum</text>
+
+</view>)
+}
+}
