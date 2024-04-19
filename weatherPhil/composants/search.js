db88f9380a1246cf5f779527cb642dcbd56ca82c
erudit aoue <philaoue@gmail.com>
2024-04-19 17:24:51 +0200

    mise en place d'une barre de recherche

---
+++ weatherPhil/composants/search.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, TextInput} from "react-native";
+
+export default function Home () {
+    return (
+    <View>
+        <TextInput
+                style={{height:40, borderColor: 'grey', borderWidth: 5}}
+              />
+    </View>
+    )
+}
