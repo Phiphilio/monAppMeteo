112ce438c5699fc60ce7f360fccce2a2b5a12adf
erudit aoue <philaoue@gmail.com>
2024-04-19 15:38:29 +0200

    mise en place des premiers styles

--- weatherPhil/composants/about.js
+++ weatherPhil/composants/about.js
@@ -1,15 +1,27 @@
 import React from "react";
-import {View,Text} from "react-native";
+import { StyleSheet,View,Text} from "react-native";
 
 export default function About () {
 
     return (
 
         <View>
-        <Text>A propos de moi</Text>
-        <Text>lorem ipsum</Text>
+        <Text style={styles.titre}>A propos de moi</Text>
+        <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus faucibus lobortis commodo. Fusce vel mattis orci, et ultricies enim. Quisque tempor dictum bibendum. Sed gravida congue enim vitae gravida. In facilisis eget nisi vel lobortis. </Text>
 
         </View>
         )
-
 }
+
+const styles = StyleSheet.create({
+
+    titre :{
+        marginBottom : 20,
+        fontSize : 22
+        },
+
+    view : {
+    margin : 20
+    }
+
+    })
