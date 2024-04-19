028f7be68d2b1238dbad1a8f89e8b20fd1158980
erudit aoue <philaoue@gmail.com>
2024-04-19 18:40:58 +0200

    je gère l'état de la valeur du champ de saisie

--- weatherPhil/composants/search.js
+++ weatherPhil/composants/search.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React , {useState} from 'react';
 import {View, Text, TextInput} from "react-native";
 
 export default function Home () {
+
+    /*constructor (props) {
+    super(props)
+    this.state = {
+    city:'Rouen'
+    }
+   }*/
+   const [city,setcity] = useState('Rouen')
     return (
     <View>
         <TextInput
                 style={{height:40, borderColor: 'grey', borderWidth: 5}}
+                placeholder = {"Rouen"}
+                onChangeText = {(text)=>setcity(text)}
+                value = {city}
               />
     </View>
     )
