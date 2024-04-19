d755cf73d3edefc76520ef03e1d58f2c61693850
erudit aoue <philaoue@gmail.com>
2024-04-19 15:43:26 +0200

    toujours la mise en place des premiers styles

--- weatherPhil/composants/about.js
+++ weatherPhil/composants/about.js
@@ -5,7 +5,7 @@ export default function About () {
 
     return (
 
-        <View>
+        <View style={styles.view}>
         <Text style={styles.titre}>A propos de moi</Text>
         <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus faucibus lobortis commodo. Fusce vel mattis orci, et ultricies enim. Quisque tempor dictum bibendum. Sed gravida congue enim vitae gravida. In facilisis eget nisi vel lobortis. </Text>
 
