72473592b33eafca57b9bf465d2c3136649debda
erudit aoue <philaoue@gmail.com>
2024-04-20 18:06:40 +0200

    je mets en place la disparition du statusBar quand on touche l'écran

--- weatherPhil/App.js
+++ weatherPhil/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import About from "./composants/about.js"
 import Home from "./composants/search.js"
-import {View, Text} from 'react-native'
+import {View, Text, StatusBar} from 'react-native'
 import {NavigationContainer} from '@react-navigation/native'
 import {createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
 
+//mise en place de la navigation
 const Tab = createMaterialTopTabNavigator ()
 
 export default function App() {
+//disoparition du statusbar (zone d'affichge du wifi)
+const [hidden, setHidden] = useState(true);
+
+const ChangeStatusBarVisibility = () => setHidden(!hidden)
   return (
-  <NavigationContainer>
+  <NavigationContainer onPress ={ChangeStatusBarVisibility}>
+  <StatusBar hidden = {hidden}/>
    <Tab.Navigator>
       <Tab.Screen name="Home" component = {Home}/>
       <Tab.Screen name="About" component = {About}/>
