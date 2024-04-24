494d0c00a3ab0bc67297277e2e8df99bb8dcbfd2
erudit aoue <philaoue@gmail.com>
2024-04-24 16:07:18 +0200

    mise en place de la requête méteo

--- weatherPhil/navigationStackScreen/searchStack.js
+++ weatherPhil/navigationStackScreen/searchStack.js
@@ -20,11 +20,21 @@ export function SearchStack () {
                 onChange = {(text)=>setcity(text)}
                 value = {city}
                 placeholder = {'rechercher une ville'}
-                onSubmitEditing = { GoResult}
+                onSubmitEditing = { WeatherReport}
               />
     </View>
     )
 }
 
+//envoie des requêtes à l'api de meteomatics
+async function WeatherReport(){
 
+    try{
+            const reponse = await fetch("https://api.meteomatics.com/2024-04-24T15:45:00.000+02:00/t_2m:C/49.4404591,1.0939658/json?").then ((reponse)=>reponse.json)
+            console.log("voici la réponse :",reponse)
+    }catch (error) {
+        console.error("voici l erreur :",error)
+    }
+
+}
 
