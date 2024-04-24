2256dad8b060228cd6b45746f2624f9b86d1e39e
erudit aoue <philaoue@gmail.com>
2024-04-24 17:20:27 +0200

    j'ai installé la dépendace buffer pour pouvoir encoder mes données

--- weatherPhil/navigationStackScreen/searchStack.js
+++ weatherPhil/navigationStackScreen/searchStack.js
@@ -3,7 +3,7 @@ import {View, Text, TextInput} from "react-native";
 import {createStackNavigator } from '@react-navigation/stack'
 import {useNavigation , NavigationContainer} from '@react-navigation/native'
 import {styleText} from '../navigation/navigationStyle.js'
-
+import { Buffer } from 'buffer'; // Importation nécessaire pour React Native
 
 
 export function SearchStack () {
@@ -31,7 +31,7 @@ async function WeatherReport(){
     const login = "snowphil_phil_phil";
     const password = "61Hahp8YJe";
     // Encodage de base 64
-    const credentials = btoa(`${username}:${password}`);
+    const credentials = Buffer.from(`${login}:${password}`).toString('base64');
 
         try{
                 const reponse = await fetch("https://api.meteomatics.com/2024-04-24T15:45:00.000+02:00/t_2m:C/49.4404591,1.0939658/json?", {
