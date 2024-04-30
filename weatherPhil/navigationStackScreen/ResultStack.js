62f8cc50ce2c4db209507c5aba7bc3c16eab9095
erudit aoue <philaoue@gmail.com>
2024-04-30 18:03:24 +0200

    tentative de récupération des propriétés contenus dans la variable end

--- weatherPhil/navigationStackScreen/ResultStack.js
+++ weatherPhil/navigationStackScreen/ResultStack.js
@@ -16,11 +16,10 @@ export function ResultStack() {
     const fetchCoordinates = async () => {
       try {
         const result = await getCoordinates(city);
-        console.log("Ce que contient result:", result);
 
         if (isMounted) {
           setCoordinates(result);
-          console.log("Ce que contient coordinates après setCoordinates:", coordinates);
+
         }
       } catch (err) {
         if (isMounted) {
@@ -37,13 +36,18 @@ export function ResultStack() {
   }, [city]);
 
   console.log("Coordonnées au rendu:", coordinates); // Voir si l'état change après le rendu
-
+  //console.log("voici la temperature:",coordinates.data[0].coordinates[0].dates[0].value)
+  const end = JSON.stringify(coordinates)
+  const [temperature, setTemperature] = useState(null)
+    setTemperature(end.data[0].coordinates[0].dates[0].value)
+  console.log("voici ce que contient end:", end)
   return (
     <View>
       {error ? (
         <Text>Erreur lors de la récupération des coordonnées : {error.message}</Text>
       ) : (
-        <Text>Coordonnées pour {city}: {coordinates ? JSON.stringify(coordinates) : 'Chargement...'}</Text>
+        <><Text>Coordonnées pour {city}: {end ? end : 'Chargement...'}</Text>
+                  <Text>la température : {end ?temperature: 'Chargement...'}</Text></>
       )}
     </View>
   );
