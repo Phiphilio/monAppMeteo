dfe7c5ead059e591780f6a176200bf0df1b7aeb8
erudit aoue <philaoue@gmail.com>
2024-05-19 20:35:45 +0200

    connexion à la abse de donnée

--- weatherPhil/BackEnd/server.js
+++ weatherPhil/BackEnd/server.js
@@ -1,14 +1,31 @@
-const express = require ('express')
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+// récupère les informations passées en variables d'environements
+dotenv.config()
+
 const app = express()
+app.use(bodyParser.json());
 const port = 5000
 
+//connexion à la base de donnée(MongoDB Atlas)
+mongoose.connect(process.env.MONGO_URI,  { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB est connectée'))
+           .catch(err => console.log(err));
+
+//routes
 app.get('/',(req,res)=>{
     res.send('hello world')
     }
   )
-
+app.post ('', (req,res)=>{
+    res.send('hello world')
+    }
+)
 app.listen(port, ()=> {
     console.log("le serveur écoute au port 5000")
-    console.log("et il écoute très bien même")
+
     }
  )
