bc62e23cc1897b1b3d19f7ac8cb1e02ddd461020
erudit aoue <philaoue@gmail.com>
2024-05-20 18:36:10 +0200

    je mets en place la route pour enyoer les infos dans la base de données

--- weatherPhil/BackEnd/routes/routes.js
+++ weatherPhil/BackEnd/routes/routes.js
@@ -1,8 +1,19 @@
 const express = require('express')
+const User = require('../schemas/user.js')
 const router = express.Router()
 
-router.post('/',(req,res)=>{
+router.post('/', async (req,res)=>{
+    const {username, password} = req.body;
+    /** on utilise la déstructuration pour retirer les données
+    username et password
+    */
+    const newUser = new User({username, password})
+    /* les données retirées sont passées en arguments de la nouvelle instance d'User*/
+
+    await newUser.save();
+    //enregistre le nouvel utilisateur dans la base de donnée
     res.send('salut je teste un truc')
+
 })
 
 
