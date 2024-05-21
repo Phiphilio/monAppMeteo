710e598f6c79b3d2f39de67af89b57880b6234b7
erudit aoue <philaoue@gmail.com>
2024-05-21 18:28:42 +0200

    message d'erreur en cas de mot de nom identique

--- weatherPhil/BackEnd/routes/routes.js
+++ weatherPhil/BackEnd/routes/routes.js
@@ -2,18 +2,30 @@ const express = require('express')
 const User = require('../schemas/user.js')
 const router = express.Router()
 
-router.post('/', async (req,res)=>{
+router.post('/newUser', async (req,res)=>{
     const {username, password} = req.body;
     /** on utilise la déstructuration pour retirer les données
     username et password
     */
-    const newUser = new User({username, password})
-    /* les données retirées sont passées en arguments de la nouvelle instance d'User*/
+    try {
+        if (!username || !password) {
+          return res.status(400).send('Username and password are required.');
+        }
+        const newUser = new User({username, password})
+        /* les données retirées sont passées en arguments de la nouvelle instance d'User*/
 
-    await newUser.save();
-    //enregistre le nouvel utilisateur dans la base de donnée
-    res.send('salut je teste un truc')
+        await newUser.save();
+        //enregistre le nouvel utilisateur dans la base de donnée
 
+        res.status(201).send('Utilisateur créé avec succès');
+        }catch(error){
+         // Vérifie si l'erreur est une duplication de clé
+            if (error.code === 11000) {
+              return res.status(400).send('Ce nom d\'utilisateur est déjà pris.');
+
+
+        }
+    }
 })
 
 
