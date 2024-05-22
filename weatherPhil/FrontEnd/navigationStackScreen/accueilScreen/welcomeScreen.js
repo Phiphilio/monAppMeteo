612be03f4e1f124176448b6576e530cc325c2311
erudit aoue <philaoue@gmail.com>
2024-05-22 15:56:42 +0200

    j'installe le bouton et le changement de stack

--- weatherPhil/FrontEnd/navigationStackScreen/accueilScreen/welcomeScreen.js
+++ weatherPhil/FrontEnd/navigationStackScreen/accueilScreen/welcomeScreen.js
@@ -1,8 +1,16 @@
-
+import { Text, Button, View} from 'react-native'
+import {useNavigation} from '@react-navigation/native'
 
 export function WelcomeStackScreen (){
+const navigation = useNavigation()
 
 return(
-    <View> A</View>
+    <View>
+     <Text>A</Text>
+    <Button
+        onPress = { ()=> { navigation.navigate('login')}}
+        title = 'Access'
+         />
+    </View>
     )
 }
