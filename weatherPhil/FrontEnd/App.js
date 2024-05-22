3ca82a964a9edcf445b08c43e5729efb484ce99c
erudit aoue <philaoue@gmail.com>
2024-05-22 15:38:46 +0200

    les 3 stackScreen sont en place

--- weatherPhil/FrontEnd/App.js
+++ weatherPhil/FrontEnd/App.js
@@ -1,48 +1,29 @@
 import React from 'react';
-import About from "./composants/about.js"
-import {SearchStack} from "./navigationStackScreen/pastScreen/searchStack.js"
-import { StatusBar, Button, View} from 'react-native'
+import { StatusBar } from 'react-native'
 import {NavigationContainer, useNavigation} from '@react-navigation/native'
 import { TabBottom,TabTop, NavigationStyle, NavigationBas,Header, SearchIcon, InfoIcon, SearchStackStyle } from './navigation/navigationStyle.js'
-
 import {createStackNavigator } from '@react-navigation/stack'
 
-import {ResultStack} from './navigationStackScreen/pastScreen/ResultStack.js'
+import {WelcomeStackScreen} from './navigationStackScreen/accueilScreen/welcomeScreen'
+import {LoginStackScreen} from './navigationStackScreen/accueilScreen/loginScreen'
+import {SigninStackScreen} from './navigationStackScreen/accueilScreen/signinScreen'
 
-
+const Stack = createStackNavigator();
 
 export default function App() {
 
   return (
 
 <NavigationContainer>
-      <StatusBar hidden = {true}/>
-        <TabBottom.Navigator screenOptions={Header}>
-            <TabBottom.Screen name = "weatherPhil" component = {TabTopScreen} options = {NavigationBas}/>
-        </TabBottom.Navigator>
+    <Stack.Navigator initialRouteName="welcome">
+        <Stack.Screen name='welcome' component ={WelcomeStackScreen}/>
+        <Stack.Screen name='login' component ={LoginStackScreen}/>
+        <Stack.Screen name='signin' component ={SigninStackScreen}/>
+    </Stack.Navigator>
   </NavigationContainer>
   );
 }
 
-export function TabTopScreen (){
-    return(
-    <TabTop.Navigator screenOptions = {NavigationStyle}>
-              <TabTop.Screen name="recherche" component = {StackScreens}  options = {SearchIcon} />
-              <TabTop.Screen name="A propos" component = {About} options = {InfoIcon} />
-           </TabTop.Navigator>)
-}
-
-
-const Stack = createStackNavigator();
-
-export function StackScreens (){
-
-return(
-        <Stack.Navigator initialRouteName="requete" >
-            <Stack.Screen name = "requete" component = {SearchStack} options ={SearchStackStyle} />
-            <Stack.Screen name = "météo" component = {ResultStack}  />
-        </Stack.Navigator>
-)}
 
 
 {/*https://www.meteomatics.com/en/api/api-tutorials/*/}
