a86c565e35031ceda6928931805ac38402e6608c
erudit aoue <philaoue@gmail.com>
2024-05-22 16:23:37 +0200

    mise en place des input

--- weatherPhil/FrontEnd/navigationStackScreen/accueilScreen/loginScreen.js
+++ weatherPhil/FrontEnd/navigationStackScreen/accueilScreen/loginScreen.js
@@ -1,8 +1,21 @@
-
-
+import { TextInput,Text, Button, View} from 'react-native'
+import {styleText} from '../../navigation/navigationStyle.js'
 export function LoginStackScreen (){
 
 return(
-    <View>B</View>
+    <View>
+    <TextInput
+    textAlign ={'left'}
+        style={styleText}
+    />
+    <TextInput
+        textAlign ={'left'}
+        style={styleText}
+    />
+    <Button
+         title = 'login'
+    />
+    <Text> signin </Text>
+    </View>
     )
 }
