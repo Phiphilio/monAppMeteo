9a6588b44c5dffac16fbdba2844ff6d8ba75d1ae
erudit aoue <philaoue@gmail.com>
2024-06-04 20:04:52 +0200

    j'ai mis à jour la requete vers l'api

--- weatherPhil/FrontEnd/navigationStackScreen/accueilScreen/loginScreen.js
+++ weatherPhil/FrontEnd/navigationStackScreen/accueilScreen/loginScreen.js
@@ -13,7 +13,7 @@ const GoSignup = ()=> { navigation.navigate('sign up')}
 //const GoConnected = ()=> { navigation.navigate('connected')}
 const GoConnected = ()=> {
     login(name,password)
-    //testConnection()
+
 }
 
 return(
@@ -45,8 +45,8 @@ return(
 
 async function login( name, password){
     const donnees = {
-        "username":"itadori yuji",
-        "password":"est le neveu de ryomen sukuna"
+        "username":name,
+        "password":password
     }
     console.log("le nom :",donnees.username)
     console.log("le mdp :",donnees.password)
@@ -55,7 +55,7 @@ async function login( name, password){
 //le problème venait du fait que mon téléphone n'écoutait pas à son port 5000, j'ai modifé sa configuration grâce à
 //cette directive donnée à l'adb : adb reverse tcp:5000 tcp:5000
 
-    const reponse = await fetch("http://localhost:5000/routes/connexion/",{
+    const reponse = await fetch("http://localhost:5000/routes/login/",{
         method : "POST",
         credentials: "include",
         headers : {
@@ -71,20 +71,3 @@ async function login( name, password){
     }
 
 }
-const testConnection = async () => {
-  try {
-    const response = await fetch("http://localhost:5000", {
-      method: "GET"
-    });
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const data = await response.text();
-    console.log("Réponse de test :", data);
-  } catch (error) {
-    console.log("Erreur de test de connexion :", error.message);
-  }
-};
-
-
-/*lorsque je crée une api en utilisant express js, est ce que je peux faire en sorte de la tester en envoyant des requetes depuis mon serveur frontend vers mon backtend ? j'essaie de le faire mais apparement, mes deux serveurs n'arrivent pas à communiquer */
